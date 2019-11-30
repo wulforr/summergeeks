1af4b534e26413fbd244db1cf6318e94a58e46b4
root <root@localhost.localdomain>
2019-11-30 16:24:26 +0530

    updated port and readme

--- node/index.js
+++ node/index.js
@@ -7,7 +7,7 @@ const sendinfo = require('./sendinfo')
 var cors = require('cors');
 app.use(cors());
 
-app.listen(5000,() => {console.log("server started")})
+app.listen(process.env.PORT || 5000,() => {console.log("server started")})
 
 app.use(express.urlencoded({ extended: false,limit: '50mb' }));
 app.use(express.json({limit: '50mb'}));
